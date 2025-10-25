b3ee37697738ff5c2867e2570706066a07688e58
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-25 14:28:36 -0600

    Add keyboard input handling in Game component and update Tile styles

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Tile } from "../Tile";
 import "./game.scss";
 
@@ -8,15 +8,49 @@ const TILES_PER_ROW = 5;
 const emptyGuesses = Array.from({ length: ROWS_PER_GAME }).map(() => null);
 
 export const Game = () => {
-  const [guesses, setGuesses] = useState(emptyGuesses);
+  // const [guesses, setGuesses] = useState<(string | null)[]>(emptyGuesses);
+  const [guesses, setGuesses] = useState<(string | null)[]>([
+    "Hel",
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [currentGuessIndex, setCurrentGuessIndex] = useState(0);
+
+  useEffect(() => {
+    const keyUpEvent = ({ key }: KeyboardEvent) => {
+      // TODO: Add validation to ensure key is valid
+      // TODO: Add backspace logic
+
+      const currentGuess = guesses[currentGuessIndex];
+      const newGuess = `${currentGuess}${key}`;
+      const newGuesses = [...guesses];
+
+      newGuesses[currentGuessIndex] = newGuess;
+
+      setGuesses(newGuesses);
+    };
+
+    window.addEventListener("keyup", keyUpEvent);
+
+    return () => {
+      window.removeEventListener("keyup", keyUpEvent);
+    };
+  }, [guesses, currentGuessIndex]);
+
+  console.log("guesses", { guesses });
 
   return (
     <div id="game">
       {Array.from({ length: ROWS_PER_GAME }).map((_, i) => (
         <div className="tile-row">
-          {Array.from({ length: TILES_PER_ROW }).map((__, j) => (
-            <Tile />
-          ))}
+          {Array.from({ length: TILES_PER_ROW }).map((__, j) => {
+            const guessForTile = guesses[i]?.[j];
+
+            return <Tile letter={guessForTile ?? undefined} />;
+          })}
         </div>
       ))}
     </div>
