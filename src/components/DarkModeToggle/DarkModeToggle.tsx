2aabf1200f87b423343f1b3f95d676898583c771
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-27 11:54:35 -0600

    Implement dark mode functionality in DarkModeToggle component
    - Integrated useLocalStorage hook to manage dark mode state.
    - Added useEffect to set initial dark mode preference based on system settings.
    - Updated checkbox input to reflect dark mode state and handle user interactions.
    - Defined DARK_MODE key in local storage constants for better organization.

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -1,9 +1,33 @@
+import { useLocalStorage } from "@uidotdev/usehooks";
 import "./dark-mode-toggle.scss";
+import { LOCAL_STORAGE_KEYS } from "../../constants";
+import { useEffect } from "react";
 
 export const DarkModeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorage(
+    LOCAL_STORAGE_KEYS.DARK_MODE,
+    false
+  );
+
+  useEffect(() => {
+    const prefersDarkMode =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    setIsDarkMode(prefersDarkMode);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div id="dark-mode-toggle">
-      <input id="toggle" type="checkbox" />
+      <input
+        id="toggle"
+        type="checkbox"
+        checked={isDarkMode}
+        onChange={({ target: { checked } }) => {
+          setIsDarkMode(checked);
+        }}
+      />
       <label htmlFor="toggle"></label>
     </div>
   );
