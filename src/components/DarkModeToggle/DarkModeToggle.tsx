88865c873725d72bcda36434e0e49fa100cbb281
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-27 11:34:57 -0600

    Add DarkModeToggle component with styles and integrate into Topbar
    - Implemented DarkModeToggle component with associated SCSS for dark mode functionality.
    - Updated DarkModeToggle to include a checkbox input and label for user interaction.
    - Adjusted Topbar styles to include padding for improved layout consistency.

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -1,5 +1,10 @@
 import "./dark-mode-toggle.scss";
 
 export const DarkModeToggle = () => {
-  return <div>dark mode toggle</div>;
+  return (
+    <div id="dark-mode-toggle">
+      <input id="toggle" type="checkbox" />
+      <label htmlFor="toggle"></label>
+    </div>
+  );
 };
