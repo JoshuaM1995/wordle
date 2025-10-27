c375b3473a45d9b2dff8ba0cf0360423c38687fe
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-27 12:21:34 -0600

    Add Font Awesome icons to Keyboard component
    - Integrated Font Awesome for the backspace icon in the Keyboard component.
    - Updated package.json and pnpm-lock.yaml to include Font Awesome dependencies.
    - Refactored icon rendering to utilize Font Awesome's FontAwesomeIcon component for improved consistency and maintainability.

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,4 +1,6 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
 import type { HandleKeyPressOptions } from "../../hooks/useGameState";
 import "./keyboard.scss";
 
@@ -78,13 +80,15 @@ export const Keyboard = ({
   // Memoize expensive calculations
   const keyStates = useMemo(() => {
     const correctWordLetters = correctWord.split("");
-    
+
     let filteredGuesses;
     if (hasWonGame) {
       filteredGuesses = guesses;
     } else if (isAnimating && lastSubmittedRow >= 0) {
       // Exclude the row that's currently animating
-      filteredGuesses = guesses.filter((_, i) => i !== currentGuessIndex && i !== lastSubmittedRow);
+      filteredGuesses = guesses.filter(
+        (_, i) => i !== currentGuessIndex && i !== lastSubmittedRow
+      );
     } else {
       filteredGuesses = guesses.filter((_, i) => i !== currentGuessIndex);
     }
@@ -129,7 +133,14 @@ export const Keyboard = ({
     });
 
     return states;
-  }, [correctWord, guesses, hasWonGame, currentGuessIndex, isAnimating, lastSubmittedRow]);
+  }, [
+    correctWord,
+    guesses,
+    hasWonGame,
+    currentGuessIndex,
+    isAnimating,
+    lastSubmittedRow,
+  ]);
 
   const getKeyClassName = useCallback(
     (key: string) => {
@@ -206,19 +217,7 @@ export const Keyboard = ({
                   });
                 }}
               >
-                <svg
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  width="20"
-                  data-testid="icon-backspace"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
-                  ></path>
-                </svg>
+                <FontAwesomeIcon icon={faDeleteLeft} />
               </div>
             );
           }
